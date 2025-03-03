8b7d8a0c0b4694409e7a899da5c68a383e98a071
amt954 <athompson283@gmail.com>
2025-03-03 03:43:03 -0800

    Updated Exercise 7-9-2.py

---
+++ Exercise 7-9-2.py
@@ -0,0 +1,26 @@
+"""Write a function named uses_none that takes a word and a string of forbidden letters, and returns True if the word
+does not use any of the forbidden letters.
+
+Here’s an outline of the function that includes two doctests. Fill in the function so it passes these tests, and add
+at least one more doctest."""
+
+def uses_none(word, forbidden):
+    """Checks whether a word avoid forbidden letters.
+    uses_none('banana', 'xyz') -> True
+    uses_none('apple', 'efg') -> False
+    """
+
+    forbidden_list = list(forbidden) # convert string to list so each character is an element
+    passed = True
+
+    # loop through each letter in "word"
+    for letter in word:
+        # compare each letter with list element (by using "in")
+        if letter in forbidden_list:
+            passed = False
+    return passed
+
+print(uses_none('banana', 'xyz')) # True
+print(uses_none('apple', 'efg')) # False
+print(uses_none('fishsticks', 'xyz')) # True
+print(uses_none('rumplestiltskin', 'efg')) # False
